f5a2ebd11d98423175cd1148dfeca577642671db
Dawid <d.ratka@exlabs.pl>
2014-06-12 14:33:41 +0200

    https workaround

--- lib/brightcove-api.rb
+++ lib/brightcove-api.rb
@@ -175,6 +175,8 @@ module Brightcove
       request = Net::HTTP::Post::Multipart.new(url.path, payload)
 
       response = Net::HTTP.start(url.host, url.port) do |http|
+        http.use_ssl = true
+        http.ssl_version = :SSLv3
         http.read_timeout = @timeout if @timeout
         http.open_timeout = @open_timeout if @open_timeout
         http.request(request)
