104d269da3aa4cb990a7709741f5acbbf08bd38e
Dawid <d.ratka@exlabs.pl>
2014-06-12 15:24:37 +0200

    https workaround

--- lib/brightcove-api.rb
+++ lib/brightcove-api.rb
@@ -172,11 +172,12 @@ module Brightcove
         payload[:file] = UploadIO.new(file, content_type, filename)
       end
 
-      request = Net::HTTP::Post::Multipart.new(url.path, payload)
-
-      response = Net::HTTP.start(url.host, url.port) do |http|
+      request = Net::HTTP::Post::Multipart.new(url.path, payload)  do
         http.use_ssl = true
         http.ssl_version = :SSLv3
+      end
+
+      response = Net::HTTP.start(url.host, url.port) do |http|
         http.read_timeout = @timeout if @timeout
         http.open_timeout = @open_timeout if @open_timeout
         http.request(request)
